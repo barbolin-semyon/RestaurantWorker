3cb503a6e9cb4affa4cb86ffc56fdf390b866867
4118BarbolinS.P <skat.apps@gmail.com>
2023-04-12 20:43:54 +0300

    git init

---
+++ app/src/main/java/com/example/restaurant/History.kt
@@ -0,0 +1,15 @@
+package com.example.restaurant
+
+import com.google.firebase.firestore.Query
+import java.util.*
+
+data class History(
+    val difference: Float,
+    val entTime: Date,
+    val startTime: Date,
+    val menu: List<Menu>,
+    val price: Float,
+    val rating: Float,
+    val table: Query,
+    val waiter: String
+)
---
+++ app/src/main/java/com/example/restaurant/Table.kt
@@ -0,0 +1,7 @@
+package com.example.restaurant
+
+data class Table(
+    val number: Int,
+    val numberOfSeats: Int,
+    val status: Boolean
+)
