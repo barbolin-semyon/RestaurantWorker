1cd5b7d08a9712fd4fcb89cfdde5d150904d3c48
4118BarbolinS.P <skat.apps@gmail.com>
2023-04-13 08:22:32 +0300

    Изменение внешнего вида

--- app/src/main/java/com/skat/restaurant/ui/features/auth/RegisterScreen.kt
+++ app/src/main/java/com/skat/restaurant/ui/features/auth/RegisterScreen.kt
@@ -1,10 +1,13 @@
 package com.skat.restaurant.ui.features.auth
 
 import android.content.Context
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -21,6 +24,7 @@ import com.skat.restaurant.ui.components.textFields.TextFieldEmail
 import com.skat.restaurant.ui.components.textFields.TextFieldPhoneNumber
 import com.skat.restaurant.ui.components.textFields.TextFieldsWithLabelError
 import com.skat.restaurant.ui.navigation.Screens
+import com.skat.restaurant.ui.theme.Orange200
 import com.skat.restaurant.utils.emailIfValid
 import com.skat.restaurant.viewModel.AuthorizationViewModel
 
@@ -44,63 +48,78 @@ fun RegisterScreen(navController: NavController) {
 
     val scrollState = rememberScrollState()
 
-    Column(
-        Modifier.fillMaxSize().verticalScroll(scrollState),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Orange200)
     ) {
-        Text(text = "Регистрация", style = MaterialTheme.typography.h5)
-
-        var email by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        var repeatPassword by remember { mutableStateOf("") }
-        var phone by remember { mutableStateOf("") }
-        var firstName by remember { mutableStateOf("") }
-        var secondName by remember { mutableStateOf("") }
-
-        TextFieldEmail(email = email, onValueChange = { email = it })
-
-        TextFieldsWithLabelError(
-            value = firstName,
-            onValueChange = { firstName = it },
-            labelText = "Введите имя",
-        )
-
-        TextFieldsWithLabelError(
-            value = secondName,
-            onValueChange = { secondName = it },
-            labelText = "Введите профиль",
-        )
-
-        TextFieldPhoneNumber(
-            value = phone,
+        Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 32.dp),
-            onValueChange = { phone = it },
-        )
+                .padding(horizontal = 16.dp),
+            elevation = 16.dp,
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Column(
+                Modifier
+                    .fillMaxWidth().padding(vertical = 16.dp)
+                    .verticalScroll(scrollState),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Регистрация", style = MaterialTheme.typography.h5)
 
-        TextFieldPassword(
-            password = password,
-            onValueChange = { password = it },
-            isError = password.length < 8 && password.isNotEmpty(),
-            errorText = "Пароль должен содержать 8 символов",
-            labelText = "Введите пароль"
-        )
+                var email by remember { mutableStateOf("") }
+                var password by remember { mutableStateOf("") }
+                var repeatPassword by remember { mutableStateOf("") }
+                var phone by remember { mutableStateOf("") }
+                var firstName by remember { mutableStateOf("") }
+                var secondName by remember { mutableStateOf("") }
 
-        TextFieldPassword(
-            password = repeatPassword,
-            onValueChange = { repeatPassword = it },
-            isError = password != repeatPassword && password.isNotEmpty(),
-            errorText = "Пароли не совпадают",
-            labelText = "Повторите пароль"
-        )
+                TextFieldEmail(email = email, onValueChange = { email = it })
 
-        Button(
-            modifier = Modifier.padding(top = 8.dp),
-            onClick = { viewModel.register(email, password) },
-            enabled = email.emailIfValid() && password.length >= 8 && password == repeatPassword,
-            content = { Text("Зарегистрироваться") }
-        )
+                TextFieldsWithLabelError(
+                    value = firstName,
+                    onValueChange = { firstName = it },
+                    labelText = "Имя",
+                )
+
+                TextFieldsWithLabelError(
+                    value = secondName,
+                    onValueChange = { secondName = it },
+                    labelText = "Фамилия",
+                )
+
+                TextFieldPhoneNumber(
+                    value = phone,
+                    onValueChange = { phone = it },
+                )
+
+                TextFieldPassword(
+                    password = password,
+                    onValueChange = { password = it },
+                    isError = password.length < 8 && password.isNotEmpty(),
+                    errorText = "Пароль должен содержать 8 символов",
+                    labelText = "Пароль"
+                )
+
+                TextFieldPassword(
+                    password = repeatPassword,
+                    onValueChange = { repeatPassword = it },
+                    isError = password != repeatPassword && password.isNotEmpty(),
+                    errorText = "Пароли не совпадают",
+                    labelText = "Подтверждение пароля"
+                )
+
+                Button(
+                    modifier = Modifier.padding(top = 8.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    onClick = { viewModel.register(email, password) },
+                    enabled = email.emailIfValid() && password.length >= 8 && password == repeatPassword,
+                    content = { Text("Зарегистрироваться") }
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/skat/restaurant/ui/features/auth/SignInScreen.kt
+++ app/src/main/java/com/skat/restaurant/ui/features/auth/SignInScreen.kt
@@ -1,20 +1,24 @@
 package com.skat.restaurant.ui.features.auth
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.restaurant.R
 import com.example.tinn.ui.components.textFields.TextFieldPassword
 import com.skat.restaurant.ui.navigation.Screens
 import com.skat.restaurant.ui.components.textFields.TextFieldEmail
+import com.skat.restaurant.ui.theme.Blue200
+import com.skat.restaurant.ui.theme.Orange200
 import com.skat.restaurant.utils.emailIfValid
 import com.skat.restaurant.viewModel.AuthorizationViewModel
 
@@ -39,34 +43,58 @@ fun SignInScreen(navController: NavHostController) {
             restoreState = true
         }
     }
-
-    Column(
-        Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Orange200)
     ) {
-        Text(text = "Авторизация", style = MaterialTheme.typography.h5)
 
-        TextFieldEmail(email = email, onValueChange = { email = it })
-
-        TextFieldPassword(
-            password = password,
-            onValueChange = { password = it },
-            isError = password.length < 8 && password.isNotEmpty(),
-            errorText = "Введите пароль",
-            labelText = "Пароль должен содержать 8 символов"
-        )
-
-
-        Button(
-            onClick = { viewModel.signInWithEmail(email, password) },
-            enabled = email.emailIfValid() && password.length >= 8,
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            elevation = 8.dp,
+            shape = RoundedCornerShape(16.dp)
         ) {
-            Text(text = "Войти")
-        }
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 32.dp, top = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Авторизация",
+                    style = MaterialTheme.typography.h5,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
 
-        TextButton(onClick = { navController.navigate(Screens.Registration.route) }) {
-            Text("Нет аккаунта? Зарегистрируйтесь")
+                TextFieldEmail(email = email, onValueChange = { email = it })
+
+                TextFieldPassword(
+                    password = password,
+                    onValueChange = { password = it },
+                    isError = password.length < 8 && password.isNotEmpty(),
+                    errorText = "Пароль должен содержать 8 символов",
+                    labelText = "Введите пароль"
+                )
+
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .padding(top = 32.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    onClick = { viewModel.signInWithEmail(email, password) },
+                    enabled = email.emailIfValid() && password.length >= 8,
+                ) {
+                    Text(text = "Войти")
+                }
+                TextButton(onClick = { navController.navigate(Screens.Registration.route) }) {
+                    Text("Нет аккаунта? Зарегистрирутесь!")
+                }
+
+            }
         }
     }
 }
