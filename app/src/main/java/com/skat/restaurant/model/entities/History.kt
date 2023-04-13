229461279e921acda8c9a365b7c4e55cee9ba724
4118BarbolinS.P <skat.apps@gmail.com>
2023-04-13 10:22:06 +0300

    Исправление сущностей

--- app/src/main/java/com/skat/restaurant/model/entities/History.kt
+++ app/src/main/java/com/skat/restaurant/model/entities/History.kt
@@ -1,16 +1,17 @@
 package com.skat.restaurant.model.entities
 
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.Query
 import com.skat.restaurant.model.entities.Menu
 import java.util.*
 
 data class History(
-    val difference: Float,
-    val entTime: Date,
-    val startTime: Date,
-    val menu: List<Menu>,
-    val price: Float,
-    val rating: Float,
-    val table: Query,
-    val waiter: String
+    val difference: Float = 0f,
+    val entTime: Date = Date(),
+    val startTime: Date = Date(),
+    val menu: List<DocumentReference> = emptyList(),
+    val price: Float = 0f,
+    val rating: Float = 0f,
+    val table: DocumentReference? = null,
+    val waiter: String = ""
 )
--- app/src/main/java/com/skat/restaurant/model/entities/Table.kt
+++ app/src/main/java/com/skat/restaurant/model/entities/Table.kt
@@ -1,7 +1,11 @@
 package com.skat.restaurant.model.entities
 
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.Query
+
 data class Table(
-    val number: Int,
-    val numberOfSeats: Int,
-    val status: Boolean
+    val number: Int = -1,
+    val numberOfSeats: Int = 0,
+    val status: Boolean = false,
+    val current: DocumentReference? = null
 )
