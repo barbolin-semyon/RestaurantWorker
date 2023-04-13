b81518889aed821da0640a89614153ff6dd1dac7
4118BarbolinS.P <skat.apps@gmail.com>
2023-04-13 08:24:12 +0300

    Добавление новых экранов

--- app/src/main/java/com/skat/restaurant/ui/navigation/Screens.kt
+++ app/src/main/java/com/skat/restaurant/ui/navigation/Screens.kt
@@ -1,5 +1,7 @@
 package com.skat.restaurant.ui.navigation
 
+import com.example.restaurant.R
+
 /**
  * Класс, включающий в себя экраны для [AppNavHost]
  */
@@ -8,3 +10,19 @@ sealed class Screens(val route: String) {
     object Registration : Screens("registration")
     object Main : Screens("main")
 }
+
+sealed class WorkScreens(val route: String) {
+    object MapScreen : WorkScreens("map")
+    object EatScreen : WorkScreens("eat")
+    object Confirmation: WorkScreens("confirm")
+}
+
+sealed class BottomScreens(val label: String, val route: String, val icon: Int) {
+    object WorkScreen : BottomScreens("Работа", "work", R.drawable.ic_work)
+    object ProfileScreen : BottomScreens("Профиль", "profile", R.drawable.ic_profile)
+}
+
+val bottomScreens = listOf(
+    BottomScreens.WorkScreen,
+    BottomScreens.ProfileScreen
+)
