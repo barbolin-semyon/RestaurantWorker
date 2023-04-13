8e68d1ccfa1779b1b024893be33f8114e5665211
4118BarbolinS.P <skat.apps@gmail.com>
2023-04-13 12:03:38 +0300

    Конструктор по умолчанию

--- app/src/main/java/com/skat/restaurant/model/entities/Menu.kt
+++ app/src/main/java/com/skat/restaurant/model/entities/Menu.kt
@@ -1,11 +1,11 @@
 package com.skat.restaurant.model.entities
 
 data class Menu(
-    val cook: String,
-    val img: String,
-    val ingredients: List<String>,
-    val name: String,
-    val price: Float,
-    val status: Boolean,
-    val time: Float
+    val cook: String = "",
+    val img: String = "",
+    val ingredients: List<String> = emptyList(),
+    val name: String = "",
+    val price: Float = 0f,
+    val status: Boolean = false,
+    val time: Float = 0f
 )
