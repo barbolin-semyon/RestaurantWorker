27185f90fa91e59450f52d3e91aef5cbc00b5c12
4118BarbolinS.P <skat.apps@gmail.com>
2023-04-13 10:22:19 +0300

    Парсер времени

--- app/src/main/java/com/skat/restaurant/utils/ExceptionFunction.kt
+++ app/src/main/java/com/skat/restaurant/utils/ExceptionFunction.kt
@@ -1,8 +1,16 @@
 package com.skat.restaurant.utils
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 /**
  * Проверка строки на валидность email-адреса
  */
 fun String.emailIfValid(): Boolean {
     return (this.isEmpty() || this.matches(Regex("\\S*@\\S*[.]\\S*")))
+}
+
+fun Date.parseDateWithTime(): String? {
+    val format = SimpleDateFormat("HH:mm:ss", Locale("ru"))
+    return format.format(this)
 }
